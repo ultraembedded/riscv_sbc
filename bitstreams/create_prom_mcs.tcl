e1350d06c546d42b3e4ccb272b5304d5c68cb053
ultraembedded <admin@ultra-embedded.com>
2020-05-10 11:08:00 +0100

    Change bootloader configuration.

--- bitstreams/create_prom_mcs.tcl
+++ bitstreams/create_prom_mcs.tcl
@@ -1,2 +1,2 @@
- write_cfgmem -force -format MCS -size 32 -interface SPIx1 -loadbit "up 0x0 fpga.bit" -loaddata "up 0x300000 primary_boot.bin up 0x400000 secondary_boot.bin" riscv_sbc.mcs
+ write_cfgmem -force -format MCS -size 32 -interface SPIx1 -loadbit "up 0x0 fpga.bit" -loaddata "up 0x300000 primary_boot.bin up 0x400000 secondary_boot.bin up 0x500000 config.dtb up 0x600000 vmlinux.bin" riscv_sbc.mcs
  quit
